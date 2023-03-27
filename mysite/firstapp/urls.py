ff0f60979a90e4a0844675f3da30f12371cdd3e0
Dimas <test@mail.ru>
2023-03-27 16:10:25 +0300

    Sixth lesson: templates

--- mysite/firstapp/urls.py
+++ mysite/firstapp/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path, re_path
-from .views import sports, title, archive, goroot
+from .views import title, about, goroot
 
 
 urlpatterns = [
     path('',  title, name='root'),
-    path('sport/<slug:sportid>/', sports),
-    re_path(r'archive/(?P<year>\d{4})/$', archive),
-    re_path(r'.*root/$', goroot)
+    path('about/', about, name='about'),
+    re_path('.*root/$', goroot)
 ]
 
 
--- mysite/firstapp/views.py
+++ mysite/firstapp/views.py
@@ -1,22 +1,22 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect
+from firstapp.models import men
 
+menu = ('О сайте', 'Добавить статью', 'Обратная связь', 'Войти')
 # Create your views here.
+
+
 def title(request):
-    return HttpResponse('<h1>welcome</h1>')
+    ps = men.objects.filter(pk__lte=3)
+    return render(request, 'firstapp/index.html', {'title': 'Главная страница', 'menu': menu, 'top': ps})
 
-def sports(request, sportid): #HttpRequest
-    if request.GET:
-        print(request.GET)
-    return HttpResponse(f'page sport: {sportid}')
 
-def archive(request, year):
-    if int(year) > 2023:
-        raise Http404
-    return HttpResponse(f'<h1>Архив за {year} год</h1>')
+def about(request):
+    return render(request, 'firstapp/about.html', {'title': 'О сайте', 'menu': menu})
 
 def goroot(request):
     return redirect('root')
+
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1><p>Вы ввели неизвестный для маршрутизатора запрос</p>')
