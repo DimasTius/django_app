04efc8bdaa3e5e9d137bc18ca158e7a847d25fc9
Dimas <test@mail.ru>
2023-03-28 16:40:49 +0300

    Eighth lesson: ---

--- mysite/firstapp/urls.py
+++ mysite/firstapp/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path, re_path
-from .views import title, about, goroot
+from .views import title, about, goroot, login, addpage, contact, show_post
 
 
 urlpatterns = [
     path('',  title, name='root'),
     path('about/', about, name='about'),
+    path('addpage/', addpage, name='add_page'),
+    path('contact/', contact, name='contact'),
+    path('login/', login, name='login'),
+    path('post/<int:post_id>/', show_post, name='post'),
     re_path(r'.*root/$', goroot)
 ]
 
--- mysite/firstapp/views.py
+++ mysite/firstapp/views.py
@@ -2,20 +2,48 @@ from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect
 from firstapp.models import men
 
-menu = ('О сайте', 'Добавить статью', 'Обратная связь', 'Войти')
+menu = [
+    {'title': 'О сайте', 'url_name': 'about'},
+    {'title': 'Добавить статью', 'url_name': 'add_page'},
+    {'title': 'Обратная связь', 'url_name': 'contact'},
+    {'title': 'Войти', 'url_name': 'login'}
+]
+
 # Create your views here.
 
 
 def title(request):
-    ps = men.objects.filter(pk__lte=3)
-    return render(request, 'firstapp/index.html', {'title': 'Главная страница', 'menu': menu, 'top': ps})
+    ps = men.objects.all()
+    context = {
+        'title': 'Главная страница',
+        'menu': menu,
+        'top': ps
+    }
+    return render(request, 'firstapp/index.html', context=context)
 
 
 def about(request):
     return render(request, 'firstapp/about.html', {'title': 'О сайте', 'menu': menu})
 
+
 def goroot(request):
     return redirect('root')
+
+
+def addpage(request):
+    return HttpResponse('Добавьте статью')
+
+
+def contact(request):
+    return HttpResponse('Оставьте сообщение')
+
+
+def login(request):
+    return HttpResponse('Ведите логин и пароль')
+
+
+def show_post(request, post_id):
+    return HttpResponse(f'Отображение статьи {post_id}')
 
 
 def pageNotFound(request, exception):
