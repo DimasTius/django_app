1f6a84a15a507a3d7f5a2f8419f2c843a4311a4d
Dimas <test@mail.ru>
2023-03-30 14:38:56 +0300

    Ten lesson: work admin, add photo. create superuser

--- mysite/firstapp/admin.py
+++ mysite/firstapp/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from firstapp.models import men, Category
+
+class menAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'time_create', 'photo', 'is_published')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'content')
+    list_editable = ('is_published', )
+    list_filter = ('is_published', 'time_create')
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', )
 
 # Register your models here.
+admin.site.register(men, menAdmin)
+admin.site.register(Category, CategoryAdmin)
